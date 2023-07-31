0078d6191ea34ab08709fbbdbdb3ea6b9351267e
win0509 <dbtmdfl1109@naver.com>
2023-07-31 20:58:09 +0900

    todolist

---
+++ src/component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+const Header = () => {
+  return (
+    <div className='Header'>
+        <h3>TODAY</h3>
+        <h1>{new Date().toDateString()}</h1>
+    </div>
+  )
+}
+
+export default Header
